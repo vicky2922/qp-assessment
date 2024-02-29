f75568998def7d2f1582ecc2802c8dff6196de2d
Vicky <bhuvavicky10@gmail.com>
2024-02-29 17:30:35 +0530

    QuestionPro submission

--- Grocery-Booking-API/src/test/java/com/questionpro/grocerybooking/GroceryBookingApplicationTests.java
+++ Grocery-Booking-API/src/test/java/com/questionpro/grocerybooking/GroceryBookingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecurityLearningJpaApplicationTests {
+class GroceryBookingApplicationTests {
 
 	@Test
 	void contextLoads() {
